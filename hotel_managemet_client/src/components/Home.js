ebdb02362902237a47c6f2b92b79d0b560599d4d
deshanviha <it19106670@my.sliit.lk>
2021-07-03 00:24:54 +0530

    frontend developing

--- hotel_managemet_client/src/components/Home.js
+++ hotel_managemet_client/src/components/Home.js
@@ -1,13 +1,39 @@
+import {useState} from 'react';
+import Backdrop from './Backdrop';
+import Modal from './Modal';
+
 function Home(props){
+
+const [modalIsopen, setModalIsOpen]  = useState(false);
+
+
+function bookingHandler(){
+
+setModalIsOpen(true);
+
+}
+
+function closeModelHandler(){
+
+        setModalIsOpen(false);
+
+}
+
 
 return(
 
 <div className='card'>
     <h2>{props.text}</h2>
         <div className='actions'>
-<button className='btn'>booking</button>
+<button className='btn' onClick={bookingHandler} >booking</button>
         </div>
+{modalIsopen && <Modal onCancel={closeModelHandler} onConfirm={closeModelHandler} />}
+{modalIsopen && <Backdrop onCancel={closeModelHandler}   />}       
+
+
 </div>
+
+
 
 );
 
---
+++ hotel_managemet_client/src/components/Modal.js
@@ -0,0 +1,25 @@
+function Modal(props){
+
+    function cancelHandler(){
+
+        props.onCancel();
+
+    }
+    function confirmHandler(){
+
+        props.onConfirm();
+
+    }
+
+
+return(
+<div className='modal'>
+
+    <p>Confirm order</p>
+    <button className='btn btn--alt' onClick={cancelHandler}>cancel</button>
+    <button className='btn'onClick={confirmHandler}>confirm</button>
+</div>
+);
+
+}
+export default Modal;
