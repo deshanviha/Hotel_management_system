d3d7a26a2f0edc8477515d3b0c5acee26eb661cd
deshanviha <it19106670@my.sliit.lk>
2021-07-08 23:39:20 +0530

    main page modification

--- hotel_managemet_client/src/components/Main.js
+++ hotel_managemet_client/src/components/Main.js
@@ -1,15 +1,32 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import vacation from '../images/pablo-merchan-montes-Orz90t6o0e4-unsplash.jpg';
-
+import './main.css';
+import {Button} from "react-bootstrap";
 function Main(){
     const myStyle = {
         width: '100%',
         height: '700px',
     };
     return(
+
+
+
+
         <div className="shoe-container">
+            <container>
+                <figure className="position-relative">
             <img src={vacation} alt="" style={myStyle} />
+                <figcaption>
+
+                    Start Your Day Here
+
+                </figcaption>
+                    <div className="book">
+                        <Button variant="danger" size="lg">BOOK NOW</Button>{}
+                    </div>
+                </figure>
+            </container>
         </div>
     )
 
