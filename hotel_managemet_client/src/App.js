0709323caf7c4b489eab578d3a8da2277d8ef108
deshanviha <it19106670@my.sliit.lk>
2021-07-09 18:55:36 +0530

    about us build

--- hotel_managemet_client/src/App.js
+++ hotel_managemet_client/src/App.js
@@ -2,6 +2,7 @@ import Home from './components/Home';
 import NavBarar from "./components/Navbar";
 import Our_specil from "./components/OurSpecial";
 import Main from "./components/Main";
+import About from "./components/About";
 
 
 function App() {
@@ -11,6 +12,7 @@ return <div>
 <NavBarar/>
 <Main/>
 <Our_specil/>
+<About/>
 
 </div>;
 }
--- hotel_managemet_client/src/components/About.js
+++ hotel_managemet_client/src/components/About.js
@@ -1,15 +1,38 @@
 import React from "react";
+import './about.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import image from '../images/patrick-tomasso-GXXYkSwndP4-unsplash.jpg'
 
 function About() {
 
+
     return (
-        <div>
-
-
+        <div className="about">
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-xs-12 col-md-6 "/>
+                    <img src={image} alt="" className="about-img"/>
+                    <div className="col-xs-12 col-md-4 ">
+                        <div className="about-text">
+                            <div className="section-title">
+                                <h2>Our Story</h2>
+                            </div>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare
+                                diam. Sed commodo nibh ante facilisis bibendum dolor feugiat at. Duis sed dapibus leo
+                                nec ornare diam commodo nibh.
+                            </p>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare
+                                diam. Sed commodo nibh ante facilisis bibendum dolor feugiat at. Duis sed dapibus leo
+                                nec ornare.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-
     )
-
 }
 
 export default About
