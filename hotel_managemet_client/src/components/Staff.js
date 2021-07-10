0c00d2cc336405722f2b312df2522d2e41517c22
deshanviha <it19106670@my.sliit.lk>
2021-07-10 14:33:48 +0530

    staff section design

---
+++ hotel_managemet_client/src/components/Staff.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './staff.css'
+
+function Staff() {
+    return (
+        <div className="staff">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        <div className="col-md-10 col-md-offset-1">
+                            <div className="section-title">
+                                <h2>
+                                    Meet Our Besties
+                                </h2>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare
+                                diam. Sed commodo nibh ante facilisis bibendum dolor feugiat at. Duis sed dapibus leo
+                                nec ornare diam commodo nibh.</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare
+                                diam. Sed commodo nibh ante facilisis bibendum dolor feugiat at. Duis sed dapibus leo
+                                nec ornare.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Staff;
