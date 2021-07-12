fe35796e7d794bcee204943a85541e0620ffca1c
deshanviha <it19106670@my.sliit.lk>
2021-07-12 22:17:15 +0530

    Offer section design

--- hotel_managemet_client/src/App.js
+++ hotel_managemet_client/src/App.js
@@ -3,17 +3,21 @@ import NavBarar from "./components/Navbar";
 import Our_specil from "./components/OurSpecial";
 import Main from "./components/Main";
 import About from "./components/About";
+import Staff from "./components/Staff";
+import Offers from "./components/Offers";
 
 
 function App() {
 
-return <div>
+    return <div>
 
-<NavBarar/>
-<Main/>
-<Our_specil/>
-<About/>
-
-</div>;
+        <NavBarar/>
+        <Main/>
+        <Our_specil/>
+        <About/>
+        <Staff/>
+        <Offers/>
+    </div>;
 }
+
 export default App;
--- hotel_managemet_client/src/components/Offers.js
+++ hotel_managemet_client/src/components/Offers.js
@@ -1,17 +1,11 @@
-import React,{useState} from 'react';
-import Carousel from 'react-bootstrap/Carousel'
-import 'gallery.css'
-import {render} from "@testing-library/react";
-function Gallery() {
-    const [index, setIndex] = useState(0);
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Carousel from "react-bootstrap/Carousel";
 
-    const handleSelect = (selectedIndex, e) => {
-        setIndex(selectedIndex);
-    };
-
+function Offers() {
     return (
-        <Carousel activeIndex={index} onSelect={handleSelect}>
-            <Carousel.Item>
+        <Carousel>
+            <Carousel.Item interval={1000}>
                 <img
                     className="d-block w-100"
                     src="holder.js/800x400?text=First slide&bg=373940"
@@ -22,13 +16,12 @@ function Gallery() {
                     <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                 </Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item>
+            <Carousel.Item interval={500}>
                 <img
                     className="d-block w-100"
                     src="holder.js/800x400?text=Second slide&bg=282c34"
                     alt="Second slide"
                 />
-
                 <Carousel.Caption>
                     <h3>Second slide label</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
@@ -40,17 +33,13 @@ function Gallery() {
                     src="holder.js/800x400?text=Third slide&bg=20232a"
                     alt="Third slide"
                 />
-
                 <Carousel.Caption>
                     <h3>Third slide label</h3>
-                    <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                    </p>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
     );
-
 }
 
-render(<Gallery />);
+export default Offers;
