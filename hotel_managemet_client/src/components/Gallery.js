236e1985d16b595f98f18007a34f3ff4c05ef97b
deshanviha <it19106670@my.sliit.lk>
2021-07-15 23:45:31 +0530

    Gallery making

--- hotel_managemet_client/src/components/Gallery.js
+++ hotel_managemet_client/src/components/Gallery.js
@@ -1,109 +1,128 @@
 import 'photoswipe/dist/photoswipe.css'
 import 'photoswipe/dist/default-skin/default-skin.css'
-import { Gallery, Item } from 'react-photoswipe-gallery'
+import {Gallery, Item} from 'react-photoswipe-gallery'
 
 const MyGallery = () => (
-    <Gallery>
-            <div></div>
-        <Item
-            original="https://placekitten.com/1024/768?image=1"
-            thumbnail="https://placekitten.com/80/60?image=1"
-            width="1024"
-            height="768"
-        >
-            {({ ref, open }) => (
-                <img ref={ref} onClick={open} src="https://placekitten.com/180/160?image=1"  alt=""/>
-            )}
-        </Item>
-        <Item
-            original="https://placekitten.com/1024/768?image=2"
-            thumbnail="https://placekitten.com/80/60?image=2"
-            width="1024"
-            height="768"
-        >
-            {({ ref, open }) => (
-                <img ref={ref} onClick={open} src="https://placekitten.com/180/160?image=2"  alt=""/>,
-                <img ref={ref} onClick={open} src="https://placekitten.com/180/160?image=2"  alt=""/>
-            )}
-        </Item>
-            <Item
-                original="https://placekitten.com/1024/768?image=2"
-                thumbnail="https://placekitten.com/80/60?image=2"
-                width="1024"
-                height="768"
-            >
-                    {({ ref, open }) => (
-                        <img ref={ref} onClick={open} src="https://placekitten.com/180/160?image=2"  alt=""/>,
-                            <img ref={ref} onClick={open} src="https://placekitten.com/180/160?image=2"  alt=""/>
-                    )}
-            </Item>
-            <Item
-                original="https://placekitten.com/1024/768?image=2"
-                thumbnail="https://placekitten.com/80/60?image=2"
-                width="1024"
-                height="768"
-            >
-                    {({ ref, open }) => (
-                        <img ref={ref} onClick={open} src="https://placekitten.com/180/160?image=2"  alt=""/>,
-                            <img ref={ref} onClick={open} src="https://placekitten.com/180/160?image=2"  alt=""/>
-                    )}
-            </Item>
-            <Item
-                original="https://placekitten.com/1024/768?image=2"
-                thumbnail="https://placekitten.com/80/60?image=2"
-                width="1024"
-                height="768"
-            >
-                    {({ ref, open }) => (
-                        <img ref={ref} onClick={open} src="https://placekitten.com/180/160?image=2"  alt=""/>,
-                            <img ref={ref} onClick={open} src="https://placekitten.com/180/160?image=2"  alt=""/>
-                    )}
-            </Item>
-            <Item
-                original="https://placekitten.com/1024/768?image=2"
-                thumbnail="https://placekitten.com/80/60?image=2"
-                width="1024"
-                height="768"
-            >
-                    {({ ref, open }) => (
-                        <img ref={ref} onClick={open} src="https://placekitten.com/180/160?image=2"  alt=""/>,
-                            <img ref={ref} onClick={open} src="https://placekitten.com/180/160?image=2"  alt=""/>
-                    )}
-            </Item>
-            <Item
-                original="https://placekitten.com/1024/768?image=2"
-                thumbnail="https://placekitten.com/80/60?image=2"
-                width="1024"
-                height="768"
-            >
-                    {({ ref, open }) => (
-                        <img ref={ref} onClick={open} src="https://placekitten.com/180/160?image=2"  alt=""/>,
-                            <img ref={ref} onClick={open} src="https://placekitten.com/180/160?image=2"  alt=""/>
-                    )}
-            </Item>
-            <Item
-                original="https://placekitten.com/1024/768?image=2"
-                thumbnail="https://placekitten.com/80/60?image=2"
-                width="1024"
-                height="768"
-            >
-                    {({ ref, open }) => (
-                        <img ref={ref} onClick={open} src="https://placekitten.com/180/160?image=2"  alt=""/>,
-                            <img ref={ref} onClick={open} src="https://placekitten.com/180/160?image=2"  alt=""/>
-                    )}
-            </Item>
-            <Item
-                original="https://placekitten.com/1024/768?image=2"
-                thumbnail="https://placekitten.com/80/60?image=2"
-                width="1024"
-                height="768"
-            >
-                    {({ ref, open }) => (
-                        <img ref={ref} onClick={open} src="https://placekitten.com/280/160?image=2"  alt=""/>,
-                            <img ref={ref} onClick={open} src="https://placekitten.com/180/160?image=2"  alt=""/>
-                    )}
-            </Item>
+    <container>
+            <Gallery>
 
-    </Gallery>
+                <Item
+                    original="https://images.unsplash.com/photo-1466978913421-dad2ebd01d17?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80"
+                    thumbnail="https://images.unsplash.com/photo-1466978913421-dad2ebd01d17?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80"
+                    width="1024"
+                    height="768"
+                >
+                    {({ref, open}) => (
+                        <img ref={ref} onClick={open}
+                             src="https://images.unsplash.com/photo-1466978913421-dad2ebd01d17?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80"
+                             alt=""/>
+                    )}
+                </Item>
+                <Item
+                    original="https://images.unsplash.com/photo-1574936145840-28808d77a0b6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80"
+                    thumbnail="https://images.unsplash.com/photo-1574936145840-28808d77a0b6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80"
+                    width="1024"
+                    height="768"
+                >
+                    {({ref, open}) => (
+
+                        <img ref={ref} onClick={open}
+                             src="https://images.unsplash.com/photo-1574936145840-28808d77a0b6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80"
+                             alt=""/>
+                    )}
+                </Item>
+                <Item
+                    original="https://images.unsplash.com/photo-1544148103-0773bf10d330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=802"
+                    thumbnail="https://images.unsplash.com/photo-1544148103-0773bf10d330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80"
+                    width="1024"
+                    height="768"
+                >
+                    {({ref, open}) => (
+
+                        <img ref={ref} onClick={open}
+                             src="https://images.unsplash.com/photo-1544148103-0773bf10d330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80"
+                             alt=""/>
+                    )}
+                </Item>
+                <Item
+                    original="https://images.unsplash.com/photo-1528605248644-14dd04022da1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+                    thumbnail="https://images.unsplash.com/photo-1528605248644-14dd04022da1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+                    width="1024"
+                    height="768"
+                >
+                    {({ref, open}) => (
+                        <img ref={ref} onClick={open}
+                             src="https://images.unsplash.com/photo-1528605248644-14dd04022da1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+                             alt=""/>
+                    )}
+                </Item>
+                <Item
+                    original="https://images.unsplash.com/photo-1587574293340-e0011c4e8ecf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1189&q=80"
+                    thumbnail="https://images.unsplash.com/photo-1587574293340-e0011c4e8ecf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1189&q=80"
+                    width="1024"
+                    height="768"
+                >
+                    {({ref, open}) => (
+                        <img ref={ref} onClick={open}
+                             src="https://images.unsplash.com/photo-1587574293340-e0011c4e8ecf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1189&q=80"
+                             alt=""/>
+                    )}
+                </Item>
+                <Item
+                    original="https://images.unsplash.com/photo-1613946069412-38f7f1ff0b65?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80"
+                    thumbnail="https://images.unsplash.com/photo-1613946069412-38f7f1ff0b65?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80"
+                    width="1024"
+                    height="768"
+                >
+                    {({ref, open}) => (
+
+                        <img ref={ref} onClick={open}
+                             src="https://images.unsplash.com/photo-1613946069412-38f7f1ff0b65?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80"
+                             alt=""/>
+                    )}
+                </Item>
+                <Item
+                    original="https://images.unsplash.com/photo-1470338745628-171cf53de3a8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+                    thumbnail="https://images.unsplash.com/photo-1470338745628-171cf53de3a8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=802"
+                    width="1024"
+                    height="768"
+                >
+                    {({ref, open}) => (
+
+                        <img ref={ref} onClick={open}
+                             src="https://images.unsplash.com/photo-1470338745628-171cf53de3a8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+                             alt=""/>
+                    )}
+                </Item>
+                <Item
+                    original="https://images.unsplash.com/photo-1498654896293-37aacf113fd9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+                    thumbnail="https://images.unsplash.com/photo-1498654896293-37aacf113fd9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+                    width="1024"
+                    height="768"
+                >
+                    {({ref, open}) => (
+                        <img ref={ref} onClick={open}
+                             src="https://images.unsplash.com/photo-1498654896293-37aacf113fd9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+                             alt=""/>
+                    )}
+                </Item>
+                <Item
+                    original="https://images.unsplash.com/photo-1502301103665-0b95cc738daf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80"
+                    thumbnail="https://images.unsplash.com/photo-1502301103665-0b95cc738daf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80"
+                    width="1024"
+                    height="768"
+                >
+                    {({ref, open}) => (
+                        <img ref={ref} onClick={open}
+                             src="https://images.unsplash.com/photo-1502301103665-0b95cc738daf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80"
+                             alt=""/>
+
+                    )}
+                </Item>
+
+            </Gallery>
+
+
+    </container>
 )
 export default MyGallery;
